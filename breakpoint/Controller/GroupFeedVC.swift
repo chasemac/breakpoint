7ffc51564ab9cf8e5005542c1d03d0099e9fe866
Chase McElroy <ichasem@gmail.com>
2018-02-01 17:24:22 -0800

    Added a scroll to most recent message in tableView

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupFeedVC: UIViewController {
     
@@ -18,7 +19,7 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var sendBtn: UIButton!
     
     var group: Group?
-    
+    var groupMessages = [Message]()
     func initData(forGroup group: Group) {
         self.group = group
     }
@@ -30,11 +31,22 @@ class GroupFeedVC: UIViewController {
             self.membersLabel.text = returnedEmails.joined(separator: ", ")
         }
         
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllMessagesFor(desiredGroup: self.group!, handler: { (returnedGroupMessages) in
+                self.groupMessages = returnedGroupMessages
+                self.tableView.reloadData()
+                
+                if self.groupMessages.count > 0 {
+                    self.tableView.scrollToRow(at: IndexPath.init(row: self.groupMessages.count - 1, section: 0), at: .none, animated: true)
+                }
+            })
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         sendBtnView.bindToKeyboard()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
@@ -42,5 +54,36 @@ class GroupFeedVC: UIViewController {
     }
     
     @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if messageTextField.text != "" {
+            messageTextField.isEnabled = false
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: messageTextField.text!, forUID: Auth.auth().currentUser!.uid, withGroupKey: group?.key, sendComplete: { (complete) in
+                if complete {
+                    self.messageTextField.text = ""
+                    self.messageTextField.isEnabled = true
+                    self.sendBtn.isEnabled = true
+                }
+            })
+        }
+    }
+}
+
+extension GroupFeedVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupMessages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupFeedCell", for: indexPath) as? GroupFeedCell else {
+            return UITableViewCell()
+        }
+        let message = groupMessages[indexPath.row]
+        DataService.instance.getusername(forUID: message.senderId) { (email) in
+            cell.configureCell(profileImage: #imageLiteral(resourceName: "defaultProfileImage"), email: email, content: message.content)
+        }
+        return cell
     }
 }
