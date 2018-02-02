28a92943530e13995bb4c9e08d5904f439d2a09b
Chase McElroy <ichasem@gmail.com>
2018-02-01 16:23:24 -0800

    Added a group feedVC to pull in all the groups and created a function to turn uid's to email addresses

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -9,19 +9,34 @@
 import UIKit
 
 class GroupFeedVC: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var groupTitleLbl: UILabel!
     @IBOutlet weak var membersLabel: UILabel!
     @IBOutlet weak var messageTextField: InsetTextField!
     @IBOutlet weak var sendBtnView: UIView!
     @IBOutlet weak var sendBtn: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-sendBtnView.bindToKeyboard()
+    
+    var group: Group?
+    
+    func initData(forGroup group: Group) {
+        self.group = group
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: self.group!) { (returnedEmails) in
+            self.membersLabel.text = returnedEmails.joined(separator: ", ")
+        }
         
     }
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sendBtnView.bindToKeyboard()
+        
+    }
+    
     @IBAction func backBtnWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
